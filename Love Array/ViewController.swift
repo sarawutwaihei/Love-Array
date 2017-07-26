1abe7d59fe0f2e216d98bd21672b0a7d44b820db
MasterUNG <phrombutr@gmail.com>
2017-07-26 11:43:42 +0700

    Add Member Array to Striing

--- Love Array/ViewController.swift
+++ Love Array/ViewController.swift
@@ -10,13 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var strArrayMember = ["Master"]
+    
+    
+    
     @IBOutlet weak var memberTextField: UITextField!
     
     @IBOutlet weak var memberLabel: UILabel!
     
     @IBAction func addButton(_ sender: Any) {
         
+        //Get Value From TextField
+        let strMember = memberTextField.text
+        print("strMember ==> \(String(describing: strMember))")
         
+        print("strArrayMember before ==> \(strArrayMember)")
+        strArrayMember.append(strMember!)
+        print("strArrayMember after ==> \(strArrayMember)")
+        
+        //Show Member
+        memberLabel.text = strArrayMember[0]
         
         
     }   // addButton
@@ -28,6 +41,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        memberLabel.sizeToFit()
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
