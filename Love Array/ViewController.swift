d16ca74a80d2915094788be8cf6158662e04f932
MasterUNG <phrombutr@gmail.com>
2017-07-26 10:15:14 +0700

    Initial Commit

---
+++ Love Array/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Love Array
+//
+//  Created by MasterUNG on 7/26/2560 BE.
+//  Copyright © 2560 MasterUNG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
