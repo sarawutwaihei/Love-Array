ba942242d575f90f1d899576b836321de6952bae
MasterUNG <phrombutr@gmail.com>
2017-07-26 11:12:52 +0700

    Design UI Success

--- Love Array/ViewController.swift
+++ Love Array/ViewController.swift
@@ -9,6 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var memberTextField: UITextField!
+    
+    @IBOutlet weak var memberLabel: UILabel!
+    
+    @IBAction func addButton(_ sender: Any) {
+        
+        
+        
+        
+    }   // addButton
+    
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
